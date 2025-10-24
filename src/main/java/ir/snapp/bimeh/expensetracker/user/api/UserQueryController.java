67bb0f74266b91f1988f01719fa5229c9f450bd4
Samane <Smn@gmail.com>
2025-10-24 18:24:22 +0330

    fix(user) : fix login issue

--- src/main/java/ir/snapp/bimeh/expensetracker/user/api/UserQueryController.java
+++ src/main/java/ir/snapp/bimeh/expensetracker/user/api/UserQueryController.java
@@ -1,15 +1,14 @@
 package ir.snapp.bimeh.expensetracker.user.api;
 
 import ir.snapp.bimeh.expensetracker.user.api.resources.LoginUserRequest;
-import ir.snapp.bimeh.expensetracker.user.api.resources.RegisterUserRequest;
-import ir.snapp.bimeh.expensetracker.user.application.command.RegisterUserCommand;
-import ir.snapp.bimeh.expensetracker.user.application.command.handler.RegisterUserHandler;
 import ir.snapp.bimeh.expensetracker.user.application.query.LoginUserCommand;
 import ir.snapp.bimeh.expensetracker.user.application.query.handler.LoginUserHandler;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.convert.ConversionService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,9 +24,13 @@ public class UserQueryController {
     private final ConversionService conversionService;
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@Valid @RequestBody LoginUserRequest request) {
+    public ResponseEntity<String> login(@Valid @RequestBody LoginUserRequest request, HttpServletRequest req) {
         LoginUserCommand convert = conversionService.convert(request, LoginUserCommand.class);
         loginUserHandler.handle(convert);
+        
+        var session = req.getSession(true);
+        session.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, SecurityContextHolder.getContext());
+        
         return ResponseEntity.ok().body("Login in successful");
     }
 }
