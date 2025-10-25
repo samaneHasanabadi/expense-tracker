1d7d4341c7ecfcec1e530a40130b5aba3dc1d138
Samane <Smn@gmail.com>
2025-10-25 22:28:29 +0330

    conf(config) : enable scheduling and refactor

--- src/main/java/ir/snapp/bimeh/expensetracker/ExpenseTrackerApplication.java
+++ src/main/java/ir/snapp/bimeh/expensetracker/ExpenseTrackerApplication.java
@@ -2,8 +2,12 @@ package ir.snapp.bimeh.expensetracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableWebSecurity
 public class ExpenseTrackerApplication {
 
     public static void main(String[] args) {
--- src/main/java/ir/snapp/bimeh/expensetracker/user/infrastructure/security/config/SecurityConfig.java
+++ src/main/java/ir/snapp/bimeh/expensetracker/user/infrastructure/security/config/SecurityConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,7 +15,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 
 @Configuration
-@EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
@@ -46,4 +44,4 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
- }
+}
